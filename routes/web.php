edbd133c1e5ca61045caf0179057a2adbb138ae2
thuynx <xuanthuy01012001@gmail.com>
2022-03-24 22:29:22 +0700

    route p1

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return 1212313;
+Route::prefix('admin')->group(function () {
+    Route::get('/unicode', function () {
+        return 'day la phuong thuc post';
+    });
+    Route::get('/show-form', function () {
+        return view('form');
+    });
+
+    Route::get('/products', function () {
+        return 'danh sach san pham';
+    });
+    Route::get('/add', function () {
+        return ' them san pham';
+    });
+
+    Route::get('/edit', function () {
+        return 'sua san pham';
+    });
+    Route::get('/delete', function () {
+        return 'xoa san pham';
+    });
 });
